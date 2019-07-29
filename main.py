c35db22bf9a926db9461d38f74e52b57dc6cb70d
Anders Milje <syntox32@gmail.com>
2019-07-29 16:47:21 +0200

    converted project to be a gcloud app

--- main.py
+++ main.py
@@ -9,17 +9,18 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['HOST'] = "0.0.0.0"
 
 js = Bundle('js/main.js', output='out/game.js')
-css = Bundle('sass/main.scss', filters='scss', output='out/main.css')
+css = Bundle('sass/main.scss', filters='libsass', output='out/main.css')
 
 assets.register('js_all', js)
 assets.register('css_all', css)
 
 @app.route('/')
 def home():
-    raw_commit = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"],
-        stdout=subprocess.PIPE).stdout.read()
-    commit = raw_commit.decode("utf-8").replace("\n", "")
-    return render_template('index.html', commit=commit)
+    #raw_commit = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"],
+    #    stdout=subprocess.PIPE).stdout.read()
+    #commit = raw_commit.decode("utf-8").replace("\n", "")
+    #return render_template('index.html', commit=commit)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', threaded=True)
