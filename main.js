252a4ab4056498038aa10085ec0a8ae6f5e70f43
Anders Milje <syntox32@gmail.com>
2025-11-09 20:24:35 +0100

    feat: simplified and made work on github pages

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 
 //import { FallingThingPool } from 'pool';
 
+const SCORE_KEY = "progress";
+const UPGRADES_KEY = "upgrades";
+
 /*
  *
  */
@@ -107,7 +110,7 @@ class FallingThingPool {
 function mobileAndTabletcheck() {
   // https://stackoverflow.com/a/11381730
   var check = false;
-  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
   return check;
 }
 
@@ -129,7 +132,7 @@ class Upgrade {
     this.pps = pps;
     this.name = name;
     this.price = price;
-    
+
     this.sprite = new PIXI.Sprite(texture);
     this.sprite.interactive = true;
     this.sprite.buttonMode = true;
@@ -169,7 +172,7 @@ class Game {
 
     this.upgrades = []
 
-     //window.innerHeight);
+    //window.innerHeight);
     // Scale mode for all textures, will retain pixelation
     //PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 
@@ -190,9 +193,9 @@ class Game {
       wordWrapWidth: 440
     });
 
-    this.texturePizza = PIXI.Texture.fromImage("static/img/pizza.png");
-    this.texturePineapple = PIXI.Texture.fromImage("static/img/pineapple.png");
-    this.textureOkHand = PIXI.Texture.fromImage("static/img/ok_hand.png");
+    this.texturePizza = PIXI.Texture.fromImage("img/pizza.png");
+    this.texturePineapple = PIXI.Texture.fromImage("img/pineapple.png");
+    this.textureOkHand = PIXI.Texture.fromImage("img/ok_hand.png");
 
     // Upgrades
     this.upgrade1 = new Upgrade(1, this.textureOkHand, 'Helping Hand', 100);
@@ -226,13 +229,13 @@ class Game {
 
   loadProgress() {
     console.log("loading progress");
-    let count = parseInt(Cookies.get("progress"));
+    let count = parseInt(localStorage.getItem(SCORE_KEY));
     this.pineappleCount = count;
     //this.setProgressText();
   }
 
   saveProgress() {
-    Cookies.set("progress", this.pineappleCount);
+    localStorage.setItem(SCORE_KEY, this.pineappleCount);
   }
 
   saveUpgrades() {
@@ -243,13 +246,13 @@ class Game {
         helpingHands++;
       }
     }
-    Cookies.set("helpingHands", helpingHands);
+    localStorage.setItem(UPGRADES_KEY, helpingHands);
 
     console.log("Saved upgrades...");
   }
 
   loadUpgrades() {
-    let helpingHands = parseInt(Cookies.get("helpingHands"));
+    let helpingHands = parseInt(localStorage.getItem(UPGRADES_KEY));
     for (let i = 0; i < helpingHands; i++) {
       this.upgrades.push(this.upgrade1);
     }
@@ -304,7 +307,7 @@ class Game {
     // i <3 javascript <3333 /s
     var self = this;
     function wrapper() { self.onBackgroundClick(); }
-    this.background.on("pointerdown", function() { wrapper(); });
+    this.background.on("pointerdown", function () { wrapper(); });
 
     this.app.stage.addChild(this.background);
   }
@@ -316,7 +319,8 @@ class Game {
   init() {
     this.canvasWrapper = document.getElementById("canvas-wrapper");
     this.app = new PIXI.Application(this.canvasWrapper.offsetWidth, 450, {
-      backgroundColor : 0x1099bb}
+      backgroundColor: 0x1099bb
+    }
     );
 
     this.initBackground();
@@ -337,7 +341,7 @@ class Game {
     // Upgrades
     var self = this;
     function upgrade1Wrapper() { self.upgrade1Click(); }
-    this.upgrade1.sprite.on("pointerdown", function() { upgrade1Wrapper(); });
+    this.upgrade1.sprite.on("pointerdown", function () { upgrade1Wrapper(); });
     this.app.stage.addChild(this.upgrade1.sprite);
 
     this.loadProgress();
@@ -415,7 +419,7 @@ class Game {
   run() {
     // Listen for animate update
     var self = this;
-    this.app.ticker.add(function(delta) {
+    this.app.ticker.add(function (delta) {
       self.tick(delta);
     });
     //var self = this;
@@ -427,7 +431,7 @@ class Game {
 let game = new Game();
 game.init();
 
-window.onresize = function(event) {
+window.onresize = function (event) {
   game.resizeApp();
 }
 
